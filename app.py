38d53221de4621d6ea0f4f242d18e698ce18e223
Curtis O <curtis@case.edu>
2023-07-04 11:58:16 -0400

    Iminitial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+
+# notes on the venv and start up
+# mac:
+#     python3 -m venv env
+#     source ./env/bin/activate
+    
+# Windows: python -m venv env
+# .\env\Scripts\activate
+
+# Both:
+#     pip install sqlalchemy
+#     pip freeze > requirements.txt
+
+# import models.py
+# main menu - add, search, analysis, exit, view 
+# functions 
+# add books to the database
+# edit books
+# delete books
+# search books
+# data clearning
+# loop running the pogram
