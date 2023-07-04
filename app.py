4716aef47f469e8c6a47d3447091cd6da0bf3039
Curtis O <curtis@case.edu>
2023-07-04 12:56:00 -0400

    added our database model

--- app.py
+++ app.py
@@ -11,7 +11,21 @@
 #     pip install sqlalchemy
 #     pip freeze > requirements.txt
 
+# git init notes
+# git remote add origin https://github.com/CurtisONeal/book-database-sql.git
+# git add .  
+# git commit -m 'Iinitial commit'
+# git push origin main/master
+
+# start up notes:
+# python3 app.py # to see the db created
+
 # import models.py
+from models import(Base, session, 
+                   Book, engine)
+
+
+
 # main menu - add, search, analysis, exit, view 
 # functions 
 # add books to the database
@@ -20,3 +34,6 @@
 # search books
 # data clearning
 # loop running the pogram
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
--- models.py
+++ models.py
@@ -1,3 +1,28 @@
+# imports
+
+from sqlalchemy import( create_engine, Column, 
+                       Integer, String, Date)
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+engine = create_engine('sqlite:///books.db',echo=True)  # creates a local database on your machine
+#This connect cimplictly makes a db at least locally if you makd a typo
+
+#Sessions keep track of everything you want to add or do
+Session = sessionmaker(bind=engine) #binds our session to the database
+session = Session()
+Base = declarative_base()
+
+class Book(Base):
+    __tablename__ = 'books'
+    id = Column(Integer, primary_key=True)
+    title = Column('Title', String) # By passing the string in, we make the db column name Title
+    author = Column('Author', String) 
+    published_date = Column('Published', Date)
+    price = Column('Price', Integer)
+    
+    def __repr__(self):
+        return f'Title: {self.title}, Author: {self.author}, Date Published: {self.date_published}, Price: {self.price}'
+
 # create a db 
 # books.db
 # create a model 
